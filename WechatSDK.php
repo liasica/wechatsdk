356cb67598d612608f9bd394224e1c1acd1ca7c5
liasica <magicrolan@qq.com>
2015-05-25 12:33:23 +0800

    更新主文件

---
+++ WechatSDK.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: liasica
+ * CreateTime: 15/5/25 下午12:32
+ * Filename: WechatSDK.php
+ * PhpStorm: v2
+ */
+namespace liasica\wechatsdk;
+use yii\base\Component;
+
+class WechatSDK extends Component
+{
+
+}
